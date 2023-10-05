d8f04d360e5883fce1d3b5a63469ac3a9091c0b5
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-10-05 20:10:14 +0545

    add: nav highlight on scroll
    create script.js with function setActiveLink() and isEndOfPage()
    
    add seperate internal scripe in index.html and download.html

---
+++ script.js
@@ -0,0 +1,44 @@
+const navFeatures = document.getElementById('navFeatures') || null
+const navDownload = document.getElementById('navDownload')
+const navContact = document.getElementById('navContact')
+
+const setActiveLink = (activeBtn) => {
+    if (activeBtn == 'navFeatures') {
+        navFeatures.classList.add('active')
+        navDownload.classList.remove('active')
+        navContact.classList.remove('active')
+    }
+    else if (activeBtn == 'navDownload') {
+        navFeatures.classList.remove('active')
+        navDownload.classList.add('active')
+        navContact.classList.remove('active')
+    }
+    else if (activeBtn == 'navContact') {
+        navFeatures.classList.remove('active')
+        navDownload.classList.remove('active')
+        navContact.classList.add('active')
+    }
+    else {
+        navFeatures.classList.remove('active')
+        navDownload.classList.remove('active')
+        navContact.classList.remove('active')
+    }
+}
+
+function isEndOfPage() {
+    const scrollTop = document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    return scrollTop + windowHeight >= documentHeight;
+}
+
+navFeatures.addEventListener('click', () => {
+    setActiveLink('navFeatures')
+})
+navDownload.addEventListener('click', () => {
+    setActiveLink('navDownload')
+})
+navContact.addEventListener('click', () => {
+    setActiveLink('navContact')
+})
+
